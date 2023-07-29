d5c05fd4b66b66a4695e49b5f38b3157ac1d9bf8
Smit kevadiya <76934359+smitkevadiya50@users.noreply.github.com>
2023-07-29 18:49:06 +0200

    Example of Polymorphisms.

--- Polymorphism.ts
+++ Polymorphism.ts
@@ -1,1 +1,35 @@
+/*
+In this example, Animal is an abstract class with an abstract method speak. Cat and Dog are subclasses that implement speak method in their own ways. We can store any subclass of Animal in an Animal array, and we can call speak method on any Animal object, regardless of its subclass. This demonstrates polymorphism.
+*/
+abstract class Animal {
+  constructor(public name: string) {}
 
+  abstract speak(): void;
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log(`${this.name} says: Meow!`);
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log(`${this.name} says: Woof!`);
+  }
+}
+
+let animals: Animal[] = [];
+animals.push(new Cat("Whiskers"));
+animals.push(new Dog("Rex"));
+
+for (let animal of animals) {
+  animal.speak();
+}
+
+/*
+Output:
+
+Whiskers says: Meow!
+Rex says: Woof!
+*/
